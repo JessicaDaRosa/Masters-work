90045f18d999b60827bb950d9f8d005d4ef45635
JessicaRosa <jessica_rosa@live.com.pt>
2020-03-09 13:24:23 +0100

    atom
    update on the atom class

--- TER/src/logic/Atom.java
+++ TER/src/logic/Atom.java
@@ -1,8 +1,9 @@
 package logic;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
-public class Atom implements Formula {
+public class Atom extends Formula {
     private String name;
 
     public void Atom(String n){
@@ -38,5 +39,59 @@ public class Atom implements Formula {
     @Override
     public int hashCode() {
         return Objects.hash(name);
+    }
+
+    public Atom(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public Interpretation getI() {
+        return super.getI();
+    }
+
+    @Override
+    public ArrayList<Node> getMarks() {
+        return super.getMarks();
+    }
+
+    @Override
+    public void setI(Interpretation i) {
+        super.setI(i);
+    }
+
+    @Override
+    public void setMarks(ArrayList<Node> marks) {
+        super.setMarks(marks);
+    }
+
+    @Override
+    public Formula getParent() {
+        return super.getParent();
+    }
+
+    @Override
+    public void setParent(Formula parent) {
+        super.setParent(parent);
+    }
+
+    @Override
+    public void mark(Node e) {
+        super.mark(e);
+    }
+
+    @Override
+    public boolean isMarkedBy(Node e) {
+        return super.isMarkedBy(e);
+    }
+
+    @Override
+    public boolean hasParent() {
+        return super.hasParent();
+    }
+
+    @Override
+    public Formula reWrite() {
+        return this;
     }
 }
