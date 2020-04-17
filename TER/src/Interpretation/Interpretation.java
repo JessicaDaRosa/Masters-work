3e084ddbaff3f634bbe3c4aa9341e92af7a78c24
SophiePaillocher <43413085+SophiePaillocher@users.noreply.github.com>
2020-04-17 16:05:13 +0200

    Update Interpretation.java

--- TER/src/Interpretation/Interpretation.java
+++ TER/src/Interpretation/Interpretation.java
@@ -7,43 +7,43 @@ import java.util.Objects;
 
 public class Interpretation {
 
-    private ArrayList<Node> tree;
-    private Node root;
+    private ArrayList<State> model;
+    private State root;
 
     public Interpretation() {
-        tree = new ArrayList<>();
+        model = new ArrayList<State>();
 
     }
 
-    public Interpretation(ArrayList<Node> tree, Node root) {
-        this.tree = tree;
+    public Interpretation(ArrayList<State> model, State root) {
+        this.model = model;
         this.root = root;
     }
 
-    public Interpretation(Node root) {
+    public Interpretation(State root) {
         this.root = root;
-        this.tree = new ArrayList<>();
+        this.model = new ArrayList<>();
     }
 
-    public Interpretation(ArrayList<Node> tree) {
-        this.tree = tree;
+    public Interpretation(ArrayList<State> model) {
+        this.model = model;
         this.root = null;
     }
 
-    public void setRoot(Node root) {
+    public void setRoot(State root) {
         this.root = root;
     }
 
-    public Node getRoot() {
+    public State getRoot() {
         return root;
     }
 
-    public ArrayList<Node> getTree() {
-        return tree;
+    public ArrayList<State> getModel() {
+        return model;
     }
 
-    public void setTree(ArrayList<Node> tree) {
-        this.tree = tree;
+    public void setModel(ArrayList<State> model) {
+        this.model = model;
     }
 
     @Override
@@ -51,33 +51,33 @@ public class Interpretation {
         if (this == o) return true;
         if (!(o instanceof Interpretation)) return false;
         Interpretation that = (Interpretation) o;
-        return getTree().equals(that.getTree()) &&
+        return getModel().equals(that.getModel()) &&
                 getRoot().equals(that.getRoot());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getTree(), getRoot());
+        return Objects.hash(getModel(), getRoot());
     }
 
     //la je me preocupe pas si la formule est reecrite ou pas
-    private ArrayList <Node> sat_(Formula f){
+    private ArrayList <State> sat_(Formula f){
         String testSub = f.toString(); //si jamais on a une formule compliquee on n'a pas a refaire le calcul.
-        ArrayList<Node> res_list = new ArrayList<>();
+        ArrayList<State> res_list = new ArrayList<>();
         if (f instanceof Negation)
         {
             //je lance sat avec la sous formule car si ce nest pas un atome la sous formule est traitée d'abor
             if(!(((Negation) f).getF() instanceof Atom))sat_(((Negation) f).getF());
             //je recupere la sous formule qui est nie et je vais tester par apport a l'opposite
             String testSub2 = ((Negation) f).getF().toString();
-            for(int i = 0; i<tree.size();i++)
+            for(int i = 0; i< model.size(); i++)
             {
                 //si le neud n'est pas marquee par la sous formule je vais le marquer avec la negation
                 //et apres je vais le mettre dans la liste de retour
-                if(!tree.get(i).isMarkedBy(testSub2))
+                if(!model.get(i).isMarkedBy(testSub2))
                 {
-                    tree.get(i).mark(testSub);
-                    res_list.add(tree.get(i));
+                    model.get(i).mark(testSub);
+                    res_list.add(model.get(i));
                 }
             }
             return res_list;
@@ -89,15 +89,15 @@ public class Interpretation {
             
             //A carré F
             if(((QopF) f).getQ() instanceof ForAll && ((QopF) f).getOp() instanceof Square){
-                ArrayList<Node> temp = new ArrayList<>();
+                ArrayList<State> temp = new ArrayList<>();
                 String testSub2 = ((QopF) f).getF().toString();
-                ArrayList<Node> toReturn = new ArrayList<>;
-                for(int i = 0; i<tree.size();i++){
-                    if(!tree.get(i).isMarkedBy(testSub2))
-                        temp.add(tree.get(i));
-                    for (Node s : res_list){
+                ArrayList<State> toReturn = new ArrayList<>();
+                for(int i = 0; i< model.size(); i++){
+                    if(!model.get(i).isMarkedBy(testSub2))
+                        temp.add(model.get(i));
+                    for (State s : res_list){
                         boolean intersectionEmpty = true;
-                        for(Node son : s.getSons()){
+                        for(State son : s.getSuccessors()){
                             if (temp.contains(son))
                                 intersectionEmpty = false;
                         }
@@ -116,12 +116,12 @@ public class Interpretation {
             if(((QopF) f).getQ() instanceof Every && ((QopF) f).getOp() instanceof Ring)
             {
                 res_list.clear();
-                for(int i = 0; i < tree.size(); i++)
+                for(int i = 0; i < model.size(); i++)
                 {
-                    if(thereIsOneInTheFuture(((QopF) f).getF(),tree.get(i)))
+                    if(thereIsOneInTheFuture(((QopF) f).getF(), model.get(i)))
                     {
-                        tree.get(i).mark(f.toString());
-                        res_list.add(tree.get(i));
+                        model.get(i).mark(f.toString());
+                        res_list.add(model.get(i));
                     }
                 }
                 return res_list;
@@ -130,15 +130,15 @@ public class Interpretation {
             if(((QopF) f).getQ() instanceof  Every && ((QopF) f).getOp() instanceof Diamond){
                 //on commence par marquer tout les sommets qui sont dans sat(F)
                 for(int i = 0; i<res_list.size();i++) {
-                    tree.get(tree.indexOf(res_list.get(i))).mark(testSub);
+                    model.get(model.indexOf(res_list.get(i))).mark(testSub);
                 }
                 //répéter jusqu'a ne plus trouver de nouvel état ie taille(res_list) soit stable
                 int sizeRes = 0;
                 while (sizeRes !=res_list.size()){
                     sizeRes=res_list.size();
                     //marquer les prédécesseurs des états marqués 
-                    for (Node s : res_list){
-                        for (Node p : s.getParents()){
+                    for (State s : res_list){
+                        for (State p : s.getPredecessors()){
                             if (! p.isMarkedBy(testSub)){
                                 p.mark(testSub);
                                 res_list.add(p);
@@ -153,14 +153,14 @@ public class Interpretation {
             {
                 //on comence par marquer tout les sommets qui sons dans sat(F)
                 for(int i = 0; i<res_list.size();i++) {
-                    tree.get(tree.indexOf(res_list.get(i))).mark(testSub);
+                    model.get(model.indexOf(res_list.get(i))).mark(testSub);
                 }
                 //recoupere tout les etats qui ne sont pas marques par ADiamond
-                ArrayList<Node> notIt = new ArrayList<>();
-                for(int i = 0; i < tree.size(); i++)
+                ArrayList<State> notIt = new ArrayList<>();
+                for(int i = 0; i < model.size(); i++)
                 {
-                    if(res_list.indexOf(tree.get(i))== -1)
-                        notIt.add(tree.get(i));
+                    if(res_list.indexOf(model.get(i))== -1)
+                        notIt.add(model.get(i));
                 }
                 //verifie que les etats non marquees peuve l'etre
                 int taille = 0;
@@ -170,7 +170,7 @@ public class Interpretation {
                     for(int i = 0; i< notIt.size(); i++)
                     {
                         if(allMyChildrenHaveIt(testSub,notIt.get(i))){
-                            tree.get(tree.indexOf(notIt.get(i))).mark(testSub);
+                            model.get(model.indexOf(notIt.get(i))).mark(testSub);
                             res_list.add(notIt.get(i));
                             notIt.remove(i);
                         }
@@ -182,8 +182,8 @@ public class Interpretation {
         }
         if(f instanceof QF1opF2)
         {
-            ArrayList<Node> satF1 = sat_(((QF1opF2) f).getF1());
-            ArrayList<Node> satF2 = sat_(((QF1opF2) f).getF2());
+            ArrayList<State> satF1 = sat_(((QF1opF2) f).getF1());
+            ArrayList<State> satF2 = sat_(((QF1opF2) f).getF2());
             // f1 ^ f2
             if(((QF1opF2) f).getOp() instanceof Conjunction && ((QF1opF2) f).getQ() == null)
             {
@@ -194,7 +194,7 @@ public class Interpretation {
                     if(satF2.indexOf(satF1.get(i)) != -1)
                     {
                         res_list.add(satF1.get(i));
-                        tree.get(tree.indexOf(satF1.get(i))).mark(testSub);
+                        model.get(model.indexOf(satF1.get(i))).mark(testSub);
                     }
                 }
             }
@@ -204,7 +204,7 @@ public class Interpretation {
                 assert satF2 != null;
                 res_list.addAll(satF2);
                 assert satF1 != null;
-                for (Node element : satF1) {
+                for (State element : satF1) {
                     if(!satF2.contains(element))res_list.add(element);
                 }
 
@@ -214,7 +214,7 @@ public class Interpretation {
                 //marquer tout les sommets dans sat(f2)
                 for(int i =0; i<satF2.size(); i++)
                 {
-                    tree.get(tree.indexOf(satF2.get(i))).mark(testSub);
+                    model.get(model.indexOf(satF2.get(i))).mark(testSub);
                     res_list.add(satF2.get(i));
                 }
                 int taille = 0;
@@ -224,13 +224,13 @@ public class Interpretation {
                     //pour tous les états dans Sat(F1), on vérifie que les successeurs sont tous marqués
                     for(int i = 0; i<satF2.size();i++){
                         boolean containAll = true;
-                        for (Node s :satF2.get(i).getSons()){
+                        for (State s :satF2.get(i).getSuccessors()){
                            if (!res_list.contains(s)) 
                                containAll = false;
                         }
                         //si tous les successeurs sont marqués, marquer l'état
                         if (containAll){
-                            tree.get(tree.indexOf(satF1.get(i))).mark(testSub);
+                            model.get(model.indexOf(satF1.get(i))).mark(testSub);
                             res_list.add(satF1.get(i));
                             satF1.remove(i);
                         }
@@ -244,7 +244,7 @@ public class Interpretation {
                 //marquer tout les sommets dans sat(f2)
                 for(int i =0; i<satF2.size(); i++)
                 {
-                    tree.get(tree.indexOf(satF2.get(i))).mark(testSub);
+                    model.get(model.indexOf(satF2.get(i))).mark(testSub);
                     res_list.add(satF2.get(i));
                 }
                 int taille = 0;
@@ -255,7 +255,7 @@ public class Interpretation {
                     {
                         if(thereIsOneInTheFuture(f, satF1.get(i)))
                         {
-                            tree.get(tree.indexOf(satF1.get(i))).mark(testSub);
+                            model.get(model.indexOf(satF1.get(i))).mark(testSub);
                             res_list.add(satF1.get(i));
                             satF1.remove(i);
                         }
@@ -266,25 +266,25 @@ public class Interpretation {
         }
         //si on ne rentre pas dans un des if on est un atome et du coup on lance le suivant:
         //parcour de tout les neuds dans l'arbre
-        for(int i = 0; i < tree.size() ; i++ )
+        for(int i = 0; i < model.size() ; i++ )
         {
-            if(tree.get(i).isMarkedBy(testSub))
-                res_list.add(tree.get(i));
+            if(model.get(i).isMarkedBy(testSub))
+                res_list.add(model.get(i));
         }
         return res_list;
     }
 
     //je vais faire une sorte de a* avec un map des sommets deja visit'es et
-    private boolean thereIsOneInTheFuture(Formula f, Node start)
+    private boolean thereIsOneInTheFuture(Formula f, State start)
     {
-        ArrayList<Node> toVisit = new ArrayList<>();
+        ArrayList<State> toVisit = new ArrayList<>();
         ArrayList<String> visited = new ArrayList<>();
-        Node position = start;
-        if(start.getSons().size() != 0)
+        State position = start;
+        if(start.getSuccessors().size() != 0)
         {
             //come ça on n'a pas a lanse 50 fois la reecriture en tring qui peut etre longue
             String form = f.toString();
-            toVisit.addAll(start.getSons());
+            toVisit.addAll(start.getSuccessors());
             while(toVisit.size() != 0)
             {
                 //on recupere le premier de la liste
@@ -299,20 +299,20 @@ public class Interpretation {
                 }
                 if(position.isMarkedBy(form))
                     return true;
-                if(position.getSons().size()!= 0)
-                    toVisit.addAll(position.getSons());
+                if(position.getSuccessors().size()!= 0)
+                    toVisit.addAll(position.getSuccessors());
                 visited.add(position.toString());
             }
         }
         return false;
     }
-    private boolean allMyChildrenHaveIt(String f, Node start)
+    private boolean allMyChildrenHaveIt(String f, State start)
     {
         //recolte des sucesseurs
         int taille = 0;
-        ArrayList<Node> toCheck = new ArrayList<>();
-        ArrayList<Node> sons = new ArrayList<>();
-        toCheck.addAll(start.getSons());
+        ArrayList<State> toCheck = new ArrayList<>();
+        ArrayList<State> sons = new ArrayList<>();
+        toCheck.addAll(start.getSuccessors());
         while (taille != toCheck.size())
         {
             taille = toCheck.size();
@@ -320,7 +320,7 @@ public class Interpretation {
             {
                 if(!toCheck.get(i).isMarkedBy(f))
                     return false;
-                sons = toCheck.get(i).getSons();
+                sons = toCheck.get(i).getSuccessors();
                 while(sons.size() != 0)
                 {
                     if(toCheck.indexOf(sons.get(0))== -1)
